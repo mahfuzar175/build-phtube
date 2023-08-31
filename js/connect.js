d56fee2810a7011c12eac0feb3f296d209b681fc
mahfuzar175 <mahfuzar175@gmail.com>
2023-09-01 01:06:28 +0600

    tab added

--- js/connect.js
+++ js/connect.js
@@ -1,1 +1,18 @@
-console.log('PHerp Tube');
+const handleCategory = async() =>{
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await response.json();
+
+    const tabContainer = document.getElementById('tab-container');
+
+    data.data.slice(0, 4).forEach((category) =>{
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <a class="btn normal-case text-base hover:bg-[#FF1F3D] hover:text-white">${category.category}</a>
+        `;
+        tabContainer.appendChild(div);
+    })
+
+    console.log(data.data);
+    
+}
+handleCategory();

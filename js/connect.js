840bc924b76a7a185fc08aa858827e105c87d486
mahfuzar175 <mahfuzar175@gmail.com>
2023-09-01 12:13:47 +0600

    time fixed

--- js/connect.js
+++ js/connect.js
@@ -10,6 +10,7 @@ const handleCategory = async() =>{
         div.innerHTML = `
         <a onclick="handleLoadCategory('${category.category_id}')" class="btn normal-case text-base hover:bg-[#FF1F3D] hover:text-white mt-4 mb-4">${category.category}</a>
         `;
+
         tabContainer.appendChild(div);
     });
 
@@ -18,27 +19,28 @@ const handleCategory = async() =>{
 };
 
 
-const handleLoadCategory = async (categoryId) =>{
+const handleLoadCategory = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
 
     const cardContainer = document.getElementById('card-container');
 
     cardContainer.innerHTML = "";
-    data.data?.forEach((content) =>{
-        
+    data.data?.forEach((content) => {
         const div = document.createElement('div');
 
         const seconds = content.others?.posted_date;
         const hours = Math.floor(seconds / 3600);
         const remainingSeconds = seconds % 3600;
         const minutes = Math.floor(remainingSeconds / 60);
-        console.log(hours, minutes)
+        console.log(hours, minutes);
+
+        const timeAgo = seconds? `<div class="text-center lg:right-4 lg:top-40 lg:absolute lg:rounded bg-[#171717] text-white p-1"><h2>${hours}hrs ${minutes} min ago</h2></div>` : '';
 
         div.innerHTML = `
         <div class="card bg-base-100 shadow-xl">
             <figure><img class="thumbnail-img w-full lg:w-[312px] lg:h-[200px] md:w-full md:h-[200px]" src="${content?.thumbnail}" /></figure>
-            <div class="text-center lg:right-4 lg:top-40 lg:absolute  rounded bg-[#171717] text-white p-1"><h2>${hours}hrs ${minutes} min ago</h2></div>
+            ${timeAgo}
             <div class="card-body">
                 <div class="flex justify-start items-center gap-1">
                     <div><img class="rounded-full w-[40px] h-[40px]" src="${content.authors[0]?.profile_picture}"></div>
@@ -46,17 +48,15 @@ const handleLoadCategory = async (categoryId) =>{
                 </div>
                 <div class="flex ml-12 gap-4">
                     <div><h2>${content.authors[0]?.profile_name}</h2></div>
-                    <div><h2>${content.authors[0].verified? '<img src="fi_10629607.svg"/>' : ''}</h2></div>
+                    <div><h2>${content.authors[0].verified ? '<img src="fi_10629607.svg"/>' : ''}</h2></div>
                 </div>
-                <div class="ml-12"><h2 >${content.others?.views}</h2></div>
+                <div class="ml-12"><h2>${content.others?.views}</h2></div>
             </div>
-            
         </div>
         `;
         cardContainer.appendChild(div);
     })
 };
-
 
 const handleBlog = () =>{
     window.location.href = 'block.html';
